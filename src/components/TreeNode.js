f089729017ceebec1f7f1b58b854f12beffdc788
Muskan jaiswal <muskanjwl@gmail.com>
2023-07-03 17:42:23 +0530

    changed to functional component

--- src/components/TreeNode.js
+++ src/components/TreeNode.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
 import { isFolderNode } from "../helper";
 import { fileContext } from "../context";
 import {
@@ -8,71 +8,52 @@ import {
 } from "react-icons/ai";
 import Checkbox from "@mui/material/Checkbox";
 
-export class TreeNode extends Component {
-  constructor(props) {
-    super(props);
+const TreeNode = ({ node }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.state = {
-      isOpen: false,
-    };
-  }
-  static contextType = fileContext;
-  componentDidUpdate(prevProps) {
-    if (prevProps.node !== this.props.node) {
-      this.forceUpdate();
-    }
-  }
-  render() {
-    const { isOpen } = this.state;
-    const { node } = this.props;
-    const { handleCheck } = this.context;
-    const hasChildren = isFolderNode(node);
-    return (
-      <div>
-        <div style={{ display: "flex" }}>
-          <Checkbox
-            size="small"
-            onChange={(e) => {
-              handleCheck(node, e);
-            }}
-            checked={node.checked === 1}
-            indeterminate={node.checked === 0.5}
-          />
-          <div
-            onClick={() =>
-              hasChildren &&
-              this.setState((prevState) => ({
-                isOpen: !prevState.isOpen,
-              }))
-            }
-            style={{ display: "flex", alignItems: "center" }}
-          >
-            {hasChildren ? (
-              isOpen ? (
-                <AiFillFolderOpen />
-              ) : (
-                <AiFillFolder />
-              )
+  const { handleCheck } = useContext(fileContext);
+  const hasChildren = isFolderNode(node);
+
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <Checkbox
+          size="small"
+          onChange={(e) => {
+            handleCheck(node, e);
+          }}
+          checked={node.checked === 1}
+          indeterminate={node.checked === 0.5}
+        />
+        <div
+          onClick={() => hasChildren && setIsOpen((prev) => !prev)}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          {hasChildren ? (
+            isOpen ? (
+              <AiFillFolderOpen />
             ) : (
-              <AiOutlineFileText />
-            )}
-            {node.name}
-          </div>
+              <AiFillFolder />
+            )
+          ) : (
+            <AiOutlineFileText />
+          )}
+          {node.name}
         </div>
-        <ul>
-          {hasChildren &&
-            isOpen &&
-            node.children.map((node, index) => {
-              return (
-                <li key={index}>
-                  <TreeNode node={node} />
-                </li>
-              );
-            })}
-        </ul>
       </div>
-    );
-  }
-}
+      <ul>
+        {hasChildren &&
+          isOpen &&
+          node.children.map((node, index) => {
+            return (
+              <li key={index}>
+                <TreeNode node={node} />
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
 
 export default TreeNode;
